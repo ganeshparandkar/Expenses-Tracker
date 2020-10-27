eaaa5fdea1e895918e43801a7d6f9911e9192c0e
Shriyash Parandkar <41060734+ganeshparandkar@users.noreply.github.com>
2020-10-28 04:18:21 +0530

    added context provider

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction></AddTransaction>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
   return (
     <>
       <h3>Add new transaction</h3>
       <form id="form">
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input
+            type="text"
+            id="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
